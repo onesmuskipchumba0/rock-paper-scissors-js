7b5dacd2a134a32d1e8b3e6d1ddd9c9756e450a2
OnesmusBett <onesmuskipchumba5@gmail.com>
2025-03-19 11:39:32 +0300

    add consitions for rock

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 
-const randomChoice = document.getElementById("random-choice");
+let randomChoice = document.getElementById("random-choice");
 const winBox = document.getElementById("win-box");
 
-// check what user has chossen
+//choose at random
+let choiceNo = Math.floor(Math.random()*3);
+const choices = [
+    {id:1, value:"Rock"},
+    {id:2, value:"Paper"},
+    {id:3, value:"Scissors"}
+]
+
+
+// check what user has chosen
 rock.addEventListener("click", ()=>{
     console.log("Rock chosen")
+    randomChoice.innerHTML = choices[choiceNo].value;
+    if(choiceNo === 0){
+        winBox.innerHTML = "Its a tie";
+    }else if(choiceNo === 1){
+        winBox.innerHTML = "You loose 😢";
+    }else{
+        winBox.innerHTML = "You won! 🥳"
+    }
 })
 paper.addEventListener("click", ()=>{
     console.log("paper chosen")
